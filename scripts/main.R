9154e0babf320fd45ad14fb1102ebcb1dfda684f
zbubster <zbu.rataj@seznam.cz>
2025-07-11 15:42:00 +0200

    added BIOTOP_SHORT, VMB works on lower level than crosswalk

--- scripts/main.R
+++ scripts/main.R
@@ -1,7 +1,7 @@
 # skript
 
 source("scripts/knihovnik.R", echo = F)
-knihovnik(c("sf", "dplyr", "terra"))
+knihovnik(c("sf", "dplyr", "stringr"))
 
 # load and prepare VMB
 vmb <- st_read("data/VMB_cliped.gpkg")
@@ -28,9 +28,25 @@ for(i in 1:length(cross$Code)){
   GT <- rbind(GT, nr)
 }
 
+# crate short version of BIOTOP_CODES
+# otherwise left_join will not work, due to lower hierarchical level within 'vmb'
+# in GT: T3.4 but in VMB: T3.4D
+vmb <- vmb %>%
+  mutate(
+    BIOTOP_SHORT = if_else( # pokud se jedna o LP, zapis LP a pokracuj
+        BIOTOP_CODES == "LP",
+        BIOTOP_CODES,
+        if_else( # pokud je na konci retezce pismeno, urizni toto pismeno a zapis zkracenou verzi, jinak zapis original
+          str_detect(BIOTOP_CODES, "[A-Z]$"),
+          str_sub(BIOTOP_CODES, 1, -2),
+          BIOTOP_CODES
+        )
+      )
+    )
+
 # join
 vmb <- vmb %>%
-  left_join(GT, by = c("BIOTOP_CODES" = "CZECH_codes"))
+  left_join(GT, by = c("BIOTOP_SHORT" = "CZECH_codes"))
 
 # write result
 st_write(vmb, "data/VMB_with_EUNIS_L2.gpkg")
